f71605dd75b05c36936cb0dc04d4837ee84a1143
Hamza Alaoui <53179016+HamzaOPLEX@users.noreply.github.com>
2020-02-11 22:51:28 +0100

    Update Extractor.py

--- Extractor.py
+++ Extractor.py
@@ -1,27 +1,28 @@
 import re
 import pyperclip
 
-# Created By HamzaOPLEX 
-
 ''' 
-    1 - Just Copy The Text you want to search in
-    2 - Run The Python Script
+        1- add all copy in a variable 
+        2- search in the variable (use re.DOTALL cause we have "\n")
+        2-1- first = search for numbers
+        2-2- second = search fo emails
 '''     
 
 text_past = pyperclip.paste()
 # search for PhoneNumbers
-phone_ReEx = re.compile(r"\d{3}[._+-]?\d{3}[._+-]?\d{4}",re.DOTALL)
+phone_ReEx = re.compile(r"\d{3}[._+-]\d{3}[._+-]\d{4}",re.DOTALL)
 phoneSearch = phone_ReEx.findall(text_past)
 for i in phoneSearch :
         print(i)
 
 # search for Emails
-email_RegEx = re.compile(r"[a-z0-9-]*@[a-z0-9]*.[a-z]*",re.DOTALL|re.I)
+email_RegEx = re.compile(r"[a-z0-9-]*@[a-z0-9]+.\w+",re.DOTALL|re.I)
 emailSearch = email_RegEx.findall(text_past)
 for i in emailSearch :
         print(i)
-# search for URL 
-Compile = re.compile(r"http[s]?://w{3}.[a-z]*.[a-z]*",re.DOTALL|re.I)
-URLsearch = Compile.findall(text_past)
+#Search For URL that srart with HTTPS / HTTP
+Compile = re.compile(r"http[s]?://(www\.)?[a-z]*.[a-z]*",DOTALL|re.I) #Or 'https?://(www\.)?\w+\.\w+'
+URLsearch = Compile.finditer(text_past)
 for i in URLsearch :
-    print(i)
+	a = str(i)
+	print(re.search("http[s]?://(www\.)?[a-z]*.[a-z]*",a).group())
